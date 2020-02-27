636eb4a973a4d962da4402a561ac4008ef29c62b
Sergio Suematsu <sergio.suematsu@digitalcode.com.br>
2020-02-27 00:05:35 -0300

    fix css grid

--- frontend/src/template/grid.jsx
+++ frontend/src/template/grid.jsx
@@ -6,10 +6,10 @@ export default class Grid extends Component {
         const cols = numbers ? numbers.split(' ') : []
         let cssClass = ''
 
-        if(cols[0]) cssClass += `col-xs-${cols[0]}`
-        if(cols[1]) cssClass += `col-sm-${cols[1]}`
-        if(cols[2]) cssClass += `col-md-${cols[2]}`
-        if(cols[3]) cssClass += `col-lg-${cols[3]}`
+        if(cols[0]) cssClass += ` col-xs-${cols[0]}`
+        if(cols[1]) cssClass += ` col-sm-${cols[1]}`
+        if(cols[2]) cssClass += ` col-md-${cols[2]}`
+        if(cols[3]) cssClass += ` col-lg-${cols[3]}`
 
         return cssClass
     }
