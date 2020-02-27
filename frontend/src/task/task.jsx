fdc4fb89e03f38f8ee6638a4cee3ec4dc59b2653
Sergio Suematsu <sergio.suematsu@digitalcode.com.br>
2020-02-26 22:52:42 -0300

    implement function add task

--- frontend/src/task/task.jsx
+++ frontend/src/task/task.jsx
@@ -1,14 +1,46 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TaskForm from './taskForm'
 import TaskList from './taskList'
 
+const urlApi = 'http://localhost:3003/api/tasks'
+
 export default class Task extends Component {
+
+
+    constructor(props){
+        super(props)
+        this.state = {description: '', list: []}
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+
+    handleAdd(){
+
+        const description = this.state.description
+        
+        axios.post(urlApi, {description})
+            .then(resp =>
+                console.log('Is it ok!')    
+            )
+        
+    }
+
+    handleChange(e){
+        this.setState({ ...this.state, description: e.target.value })
+    }
+
     render(){
         return (
             <div>
                 <PageHeader name='Tasks' small='Create' />
-                <TaskForm />
+                <TaskForm 
+                    description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd}/>
                 <TaskList />
             </div>
         )
--- frontend/src/task/taskForm.jsx
+++ frontend/src/task/taskForm.jsx
@@ -8,10 +8,12 @@ export default props => (
 
         <Grid cols='12 9 10'>
             <input id='description' className='form-control' 
-                placeholder='Add Task' /> 
+                placeholder='Add Task' 
+                onChange={props.handleChange}
+                value={props.description}/> 
         </Grid>
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'></IconButton>
+            <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
         </Grid>       
     </div>
 
